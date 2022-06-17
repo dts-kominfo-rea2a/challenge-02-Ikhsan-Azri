ac9cef02d10a94ed5537961958999bec1195332a
Ikhsan Azri <ikhsanazri99@gmail.com>
2022-06-17 07:03:20 +0700

    done

---
+++ test.js
@@ -0,0 +1,10 @@
+const arrayObjectPegawai = require("./data-customer.json");
+
+let jsonArr = JSON.parse(arrayObjectPegawai);
+ 
+let hasilLooping = null;
+for (let i=0; i<=jsonArr.lenght-1; i++ ){
+
+}
+
+console.log(arrayObjectPegawai.lenght);
